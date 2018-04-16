266ced40a6d0cb6c5c898cac2ce1a4ac067613ea
MarioAlbo <mario.albo@est.fib.upc.edu>
2018-04-16 08:49:41 +0200

    Create Jugador.java

---
+++ Jugador.java
@@ -0,0 +1,8 @@
+public class Jugador {
+    public int userID;
+    public int password;
+    public Jugador (int usrid, int psw) {
+        this.userID = usrid;
+        this.password = psw;
+    }
+}
