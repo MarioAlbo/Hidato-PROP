7b9cd56d1a6fabef524b31efa16015d82954d230
MarioAlbo <mario.albo@est.fib.upc.edu>
2018-04-30 12:08:09 +0200

    Update Usuari.java

--- Usuari.java
+++ Usuari.java
@@ -2,26 +2,47 @@ public class Usuari {
     private String nikname;
     private String psw;
 
+    /**
+     * dona el nom de l'usuari
+     * @return: retorna el nick de l'usari en forma de String
+     */
     public String getnikname(){
         return nikname;
     }
 
+    /**
+     * dona la contrasenya de l'usuari
+     * @return: retorna psw de l'usuari
+     */
     public String getPsw(){
         return psw;
     }
 
+    /**
+     * imprimeix i dona els paramentres de la instacia de la clase Usuari
+     * @param nom: nick de l'objecte usuari
+     * @param contraseña: psw de l'objecte usuari
+     */
     public Usuari(String nom, String contraseña){
         System.out.println("Nuevo usuario: "+nom+" "+contraseña);
         nikname = nom;
         psw = contraseña;
     }
 
+    /**
+     * canvia el valor del parametre psw de la instancia d'Usuari
+     * @param contraseña: nou valor del parametre psw
+     */
     public void cambiarcontraseña(String contraseña){
         psw = contraseña;
     }
 
+    /**
+     * canvia el valor del parametre nicke de la instacia d'Usuari
+     * @param nom: nou valor del parametre de la instancia de la classe Usuari
+     */
     public void cambiarnikname(String nom){
         nikname = nom;
     }
 }
- 
+
