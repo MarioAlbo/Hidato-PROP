5b6974a335b3308096139b1ee35b6870bc8b5e20
MarioAlbo <mario.albo@est.fib.upc.edu>
2018-06-03 21:47:17 +0200

    Add files via upload

--- Ctrl_Domini.java
+++ Ctrl_Domini.java
@@ -3,6 +3,10 @@ import com.google.gson.Gson;
 public class Ctrl_Domini {
     Ctrl_Persistencia cp;
 
+    /**
+     * Guarda en la capa de persistencia la instancia de la clase Conj_partida en forma de String
+     * @param conj_partida objeto a guardad en la capa de persistencia
+     */
     public void guardar_partida(Conj_partida conj_partida) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
@@ -10,6 +14,10 @@ public class Ctrl_Domini {
         cp.guardar_CP(conj);
     }
 
+    /**
+     * Devuelve el objeto Conj_partida guardado en la capa de persistencia en forma de String
+     * @return devuelve la instancia de la clase Conj_partida que se encuentra en la capa de persistencia
+     */
     public Conj_partida cargar_ConjPartida() {
         cp = new Ctrl_Persistencia();
         cp.cargar_CP();
@@ -17,6 +25,10 @@ public class Ctrl_Domini {
         return gson.fromJson(cp.cargar_CP(),Conj_partida.class);
     }
 
+    /**
+     * Guarda una instancia de la clase Usuari en forma de String en la capa de persistencia
+     * @param usuari objecto que será convertido a String para ser guardado
+     */
     public void guardar_usuari(Usuari usuari) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
@@ -24,12 +36,21 @@ public class Ctrl_Domini {
         cp.guardar_usuari(u, usuari.getnikname());
     }
 
+    /**
+     * Devuelve un objeto Usuari guardado en la capa de persistencia en forma de String
+     * @param nick Identificador del objecto a devolver
+     * @return devuelve una instancia de la clase Usuari que se encuentra en la capa de persistencia
+     */
     public Usuari cargar_usuari(String nick) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
         return gson.fromJson(cp.cargar_usuari(nick), Usuari.class);
     }
 
+    /**
+     * Guarda una instancia de la clase Hidato en forma de String en la capa de persistencia
+     * @param hidato objecto que será convertido a String para ser guardado
+     */
     public void guardar_hidato(Hidato hidato) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
@@ -37,17 +58,30 @@ public class Ctrl_Domini {
         cp.guardar_hidato(h,hidato.getIdH());
     }
 
+    /**
+     * Devuelve un objeto Hidato guardado en la capa de persistencia en forma de String
+     * @param idH Identificador del objecto a devolver
+     * @return devuelve una instancia de la clase Hidato que se encuentra en la capa de persistencia
+     */
     public Hidato cargar_hidato(Integer idH) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
         return gson.fromJson(cp.cargar_hidato(idH), Hidato.class);
     }
 
+    /**
+     * Elimina de la capa de persistencia un objecto de la clase Hidato identificado por idH
+     * @param idH identificador del objecto Hidato
+     */
     public void borrar_hidato(Integer idH) {
         cp = new Ctrl_Persistencia();
         cp.borrar_hidato(idH);
     }
 
+    /**
+     * Guarda una instancia de la clase Taulell en forma de String en la capa de persistencia
+     * @param taulell objecto que será convertido a String para ser guardado
+     */
     public void guardar_taulell(Taulell taulell) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
@@ -55,12 +89,21 @@ public class Ctrl_Domini {
         cp.guardar_taulell(t, taulell.getIdT());
     }
 
+    /**
+     * Devuelve un objeto Taulell guardado en la capa de persistencia en forma de String
+     * @param idt identificador del objeto a devolver
+     * @return devuelve una instancia de la clase Taulell que se encuentra en la capa de persistencia
+     */
     public Taulell cargar_taulell(Integer idt) {
         cp = new Ctrl_Persistencia();
         Gson gson = new Gson();
         return gson.fromJson(cp.cargar_taulell(idt), Taulell.class);
     }
 
+    /**
+     * Elimina de la capa de persistencia un objecto de la clase Taulell identificado por idH
+     * @param idt identificador del objecto Taulell
+     */
     public void borrar_taulell(Integer idt) {
         cp = new Ctrl_Persistencia();
         cp.borrar_taulell(idt);
--- Ctrl_Persistencia.java
+++ Ctrl_Persistencia.java
@@ -15,60 +15,106 @@ public class Ctrl_Persistencia {
     private String path_taulell = "C:\\Users\\Mario\\IdeaProjects\\prova\\src\\Persistencia\\fichero_taulell_";
     private PersistenciaTaulell pt;
 
+    /**
+     * Recibe un objecto Conj_partida en forma de String para que Persistencia_partida lo guarde en un fichero
+     * @param cp objecto Conj_partida convertido a String
+     */
     public void guardar_CP(String cp) {
         pp = new PersistenciaPartida();
         File conj_partides_fitxer = new File(path_CP);
         pp.guardar_CP(cp, conj_partides_fitxer);
     }
 
+    /**
+     * Recibe un String que corresponde al objecto de la clase Conj_partides y lo envia a Ctrl_Domini
+     * @return devuelve el objeto de Conj_partida en forma de string a la capa de Domini
+     */
     public String cargar_CP() {
         pp = new PersistenciaPartida();
         File conj_partides_fitxer = new File(path_CP);
         return pp.cargar_CP(conj_partides_fitxer);
     }
 
+    /**
+     * Recibe un objecto Usuari en forma de String para que Persistencia_partida lo guarde en un fichero
+     * @param u objecto Usuari en forma de String
+     * @param path nickname del usuario para añadir al nombre del fichero que guarde String u
+     */
     public void guardar_usuari(String u, String path) {
         pu = new PersistenciaUsuari();
         File usuari_fitxer = new File(path_usuario + path);
         pu.guardar_U(u, usuari_fitxer);
     }
 
+    /**
+     * Recibe un String que corresponde al objecto de la clase Usuari y lo envia a Ctrl_Domini
+     * @param s nickname del usuario que lo identifica para cargarlo
+     * @return devuelve un String que corresponde al objecto Usuario identificado por String s
+     */
     public String cargar_usuari(String s) {
         pu = new PersistenciaUsuari();
         File usuari_fitxer = new File(path_usuario + s);
         return pu.cargar_U(usuari_fitxer);
     }
 
+    /**
+     * recibe un String y lo envia a PersistenciaHidato para que guarde este en un fichero
+     * @param h objecto Hidato convertido a String
+     * @param path Integer que se añadirá al final del fichero ppara identificar que pertenece al objecto h
+     */
     public void guardar_hidato(String h, Integer path) {
         ph = new PersistenciaHidato();
         File hidato_fitxer = new File(path_hidato + path);
         ph.guardar_H(h, hidato_fitxer);
     }
 
+    /**
+     * devuelve a la capa de Domini en forma de String un objecto de la clase Hidato
+     * @param idH Identificador del Hidato
+     * @return devuelve un String que corresponde a un objecto de la clase Hidato
+     */
     public String cargar_hidato(Integer idH) {
         ph = new PersistenciaHidato();
         File hidato_fitxer = new File(path_hidato + idH);
         return ph.cargar_H(hidato_fitxer);
     }
 
+    /**
+     * elimina de la capa de persistencia el Hidato identificado por path
+     * @param path Identificador del Hidato a borrar
+     */
     public void borrar_hidato(Integer path) {
         ph = new PersistenciaHidato();
         File file_hidato = new File(path_hidato + path);
         ph.borrar_hidato(file_hidato);
     }
 
+    /**
+     * guarda en la capa de persistencia una instancia de la clase Taulell
+     * @param t String que corresponde a un objeto Taulell
+     * @param idt Identificador del objeto que se guardará en la capa de persistencia
+     */
     public void guardar_taulell(String t, Integer idt) {
         pt = new PersistenciaTaulell();
         File taulell_fitxer = new File(path_taulell + idt);
         pt.guardar_T(t, taulell_fitxer);
     }
 
+    /**
+     * devuelve a la capa de Domini en forma de String un objecto de la clase Taulell
+     * @param idt Integer que identifica al objeto que se quiere cargar
+     * @return devuelve un String que corresponde a un objecto de la clase Taulell
+     */
     public String cargar_taulell(Integer idt) {
         pt = new PersistenciaTaulell();
         File taulell_fitxer = new File(path_taulell + idt);
         return pt.cargar_T(taulell_fitxer);
     }
 
+    /**
+     * elimina de la capa de persistencia el Hidato identificado por idt
+     * @param idt Identificador del Taulell a borrar
+     */
     public void borrar_taulell(Integer idt) {
         pt = new PersistenciaTaulell();
         File file_taulell = new File(path_taulell + idt);
